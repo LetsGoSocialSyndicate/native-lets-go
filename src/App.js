228a2b938ffc34bc9a60cc582ab3eda7a4b54237
slledru <slledru@msn.com>
2018-06-23 17:34:26 -0600

    common components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Header } from './components/common'
+
+class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={'Let\'s Go, The App'}/>
+        <Text>Lets Go, The App</Text>
+      </View>
+    )
+  }
+}
+
+export default App
