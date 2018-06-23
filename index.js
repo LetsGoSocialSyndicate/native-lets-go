735e7cdfb9f477bd45462fc841260c56e9975abe
slledru <slledru@msn.com>
2018-06-23 09:27:49 -0600

    initial react native app setup

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import a library to help create a Component
+import React from 'react'
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('app', () => App);
