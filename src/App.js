f331fd119c974f875d9cba49af0a893feb3286eb
Tanya <mellene@gmail.com>
2018-06-23 22:52:01 -0600

    added npm packages and fixed app path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Header headerText={'Let\'s Go, The App'}/>
+        <Header headerText={'Let\'s Go, The App'} />
         <Text>Lets Go, The App</Text>
       </View>
     )
