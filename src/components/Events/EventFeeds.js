0261ad825897b7008ca9b920cb853589e3fb30ff
slledru <slledru@msn.com>
2018-07-13 05:48:56 -0600

    new test bundle created

--- src/components/Events/EventFeeds.js
+++ src/components/Events/EventFeeds.js
@@ -4,24 +4,27 @@
  import React from 'react'
  import { Actions } from 'react-native-router-flux'
  import { connect } from 'react-redux'
+ import { Container } from 'native-base'
 
  import { Button, Card, CardSection } from '../common'
  import { logout } from '../../actions/authAction'
 
 const EventFeeds = ({ logoutAction }) => {
   return (
-    <Card>
-      <CardSection>
-        <Button onPress={() => Actions.profile({ forOtherUser: false })}>
-          Profile
-        </Button>
-      </CardSection>
-      <CardSection>
-        <Button onPress={logoutAction}>
-          Logout
-        </Button>
-      </CardSection>
-    </Card>
+    <Container>
+      <Card>
+        <CardSection>
+          <Button onPress={() => Actions.profile({ forOtherUser: false })}>
+            Profile
+          </Button>
+        </CardSection>
+        <CardSection>
+          <Button onPress={logoutAction}>
+            Logout
+          </Button>
+        </CardSection>
+      </Card>
+    </Container>
   )
 }
 
